fb90f151e2461b4dd8f6f4f82a5f98d8aeee014e
shafayetullah7 <shafayet769@gmail.com>
2024-06-12 20:38:00 +0600

    changess

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.use("/api/v1", router);
+// app.use("/api/v1", router);
 app.use("/api/v2", testRouter);
 // admin
 // app.use("/api/v1/admin/account/", adminAccountRouter);
